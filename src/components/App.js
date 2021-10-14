ad4d53979ba8b067b7f4e3d54efa1d15f2af3b13
dimitris <mitsos-r21@hotmail.gr>
2021-10-15 00:15:28 +0200

    Add MUI

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
 
 const App = () => {
-	return (
-		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
-	)
-}
+  return (
+    <div className="app">
+      <h1>React Electron Boilerplate</h1>
+      <p>This is a simple boilerplate for using React with Electron</p>
+      <Button variant="contained">Hello</Button>
+    </div>
+  );
+};
 
-export default App
+export default App;
